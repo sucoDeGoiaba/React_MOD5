71810ea96ea34873e4a8f195fa5e3acb9266255b
fabianobarroli <fabianobarroli@gmail.com>
2022-04-21 20:29:00 -0300

    feat (src\components\Cards): criando arquivo CardsSyled do componente Card

---
+++ react-mod5/src/components/Cards/CardsSytled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div
+`
+width: 400px;
+heigth: 250px;
+border-radius: 15px;
+margin: 5px;
+padding; 5px;
+display: felx;
+flex-direction: column;
+justify-content: space-evenly;
+align-items: center;
+p{
+    font-family: "Comic Sans MS", Comic Sans, cursive;
+}
+`
+
