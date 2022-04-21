89b43f3281cc59f035bed892657edde2a0e53e74
fabianobarroli <fabianobarroli@gmail.com>
2022-04-21 17:58:53 -0300

    feat (src/App.jsx): colocando imports necessários

--- react-mod5/src/App.jsx
+++ react-mod5/src/App.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import BuscaItens from "./pages/BuscaItens/BuscaItens.jsx";
+
+
 
 function App() {
   return (
