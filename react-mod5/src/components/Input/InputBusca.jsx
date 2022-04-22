e8cd5172ed00b3760b529cc40945035e1e1fdb56
fabianobarroli <fabianobarroli@gmail.com>
2022-04-21 21:47:57 -0300

    feat (src\components\Input): criando o componente Input e busca

---
+++ react-mod5/src/components/Input/InputBusca.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ContainerBusca, InputBusca } from "./InputBuscaStyled.jsx";
+
+
+function InputBuscaId({id, onSubmit, type, value}) {
+    return(
+        <ContainerBusca>
+            <InputBusca/>
+        </ContainerBusca>
+    );
+};
+
+export default InputBuscaId;
---
+++ react-mod5/src/components/Input/InputBuscaStyled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ContainerBusca = styled.div
+`
+display: flex;
+flex-direction:column;
+justify-content: space-around;
+`;
+
+export const InputBusca = styled.input
+`
+width: 80%;
+padding: 10px 20px;
+margin-bottom:20px;
+`;
+ 
