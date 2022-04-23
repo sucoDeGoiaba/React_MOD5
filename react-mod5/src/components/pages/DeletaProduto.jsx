260d55c67eaa8944010d28da7fad92e0bf15c873
Vivian Caroline M <viviancaroliner@gmail.com>
2022-04-22 23:29:30 -0300

    feat: integrando rota delete à API

---
+++ react-mod5/src/components/pages/DeletaProduto.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useParams } from "react-router-dom";
+import Button from "../button/Button";
+
+function DeletaProduto() {
+  const { id } = useParams();
+  const [produto, setProduto] = useState({});
+
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   api.get("/produtos/" + id).then((response) => {
+  //     const dados = response.data;
+  //     console.log(dados);
+  //     setProduto(dados.produto.produto[0]);
+  //   });
+  // }, [id]);
+
+  function handleDelete() {
+    api.delete("/produto/" + produto.id);
+    // navigate("/");
+  }
+
+  return (
+    <div>
+      <Button nome="Deletar produto" click={() => handleDelete()} />
+    </div>
+  );
+}
+
+export default DeletaProduto;
