8dd1f05b5c66bde3758b51b25010b175b66cae73
Vivian Caroline M <viviancaroliner@gmail.com>
2022-04-22 23:28:15 -0300

    feat: inserindo o button deletar

--- react-mod5/src/components/form/Form.jsx
+++ react-mod5/src/components/form/Form.jsx
@@ -1,28 +1,40 @@
-import { useState } from "react"
-import { api } from "../../services/api.js"
-import Input from "../input/Input.jsx"
+import { useState } from "react";
+import { api } from "../../services/api.js";
+import Input from "../input/Input.jsx";
+import DeletaProduto from "../pages/DeletaProduto.jsx";
 
-export default function Form (){
-    const [prodInfo, setProdInfo] = useState({})
+export default function Form() {
+  const [prodInfo, setProdInfo] = useState({});
 
-    // Pega a info dos Inputs e passa um JSON para prodInfo, contendo nome e valor do Input para futuro envio
-    function onChange(e){
-        setProdInfo({...prodInfo, [e.target.name] : e.target.value})
-    }
+  // Pega a info dos Inputs e passa um JSON para prodInfo, contendo nome e valor do Input para futuro envio
+  function onChange(e) {
+    setProdInfo({ ...prodInfo, [e.target.name]: e.target.value });
+  }
 
-    // Envia as informações do produto para API
-    function submit(e){
-        e.preventDefault()
-        api.post("/addProd", prodInfo)
-    }
+  // Envia as informações do produto para API
+  function submit(e) {
+    e.preventDefault();
+    api.post("/addProd", prodInfo);
+  }
 
-    return(
-        <form onSubmit={submit}>
-            <Input labelName="Nome do Produto:" name="nomeProd" id="nomeProd" onChange={onChange}/>
-            <Input labelName="Quantidade:" name="quantidade" id="quantidade" onChange={onChange}/>
-            <Input labelName="Preço:" name="preco" id="preco" onChange={onChange}/>
-            <Input labelName="Tipo:" name="tipo" id="tipo" onChange={onChange}/>
-            <button type="submit">Adicionar</button>
-        </form>
-    )
+  return (
+    <form onSubmit={submit}>
+      <Input
+        labelName="Nome do Produto:"
+        name="nomeProd"
+        id="nomeProd"
+        onChange={onChange}
+      />
+      <Input
+        labelName="Quantidade:"
+        name="quantidade"
+        id="quantidade"
+        onChange={onChange}
+      />
+      <Input labelName="Preço:" name="preco" id="preco" onChange={onChange} />
+      <Input labelName="Tipo:" name="tipo" id="tipo" onChange={onChange} />
+      <button type="submit">Adicionar</button>
+      <DeletaProduto />
+    </form>
+  );
 }
