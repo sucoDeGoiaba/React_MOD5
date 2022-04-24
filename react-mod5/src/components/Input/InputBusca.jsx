5c86d4af1211331be50a3425d6eb3d0174403344
fabianobarroli <fabianobarroli@gmail.com>
2022-04-24 08:10:12 -0300

    feat - alterando arquivo

--- react-mod5/src/components/Input/InputBusca.jsx
+++ react-mod5/src/components/Input/InputBusca.jsx
@@ -3,6 +3,18 @@ import { ContainerBusca, InputBusca } from "./InputBuscaStyled.jsx";
 
 
 function InputBuscaId({id, onSubmit, type, value}) {
+    const [produto, setProduto]  = useState([]);
+    const navigate = useNavigate();
+    const {id} = useParams();
+    
+    useEffect(()=>{
+        api.get(`/id/{id}`)
+        .then((response)=>{
+            setProduto(response.data.estoque[0]);
+        })
+        .catch((erro)=> console.log(erro))
+    }, []);
+   
     return(
         <ContainerBusca>
             <InputBusca/>
