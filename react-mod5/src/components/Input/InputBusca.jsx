794be23649eeecdb51eebe73bb683d742e44ac92
fabianobarroli <fabianobarroli@gmail.com>
2022-04-24 08:29:00 -0300

    feat (components\froms) criando arquivo

--- react-mod5/src/components/Input/InputBusca.jsx
+++ react-mod5/src/components/Input/InputBusca.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { ContainerBusca, InputBusca } from "./InputBuscaStyled.jsx";
 
-
-function InputBuscaId({id, onSubmit, type, value}) {
-    const [produto, setProduto]  = useState([]);
-    const navigate = useNavigate();
-    const {id} = useParams();
-    
-    useEffect(()=>{
-        api.get(`/id/{id}`)
-        .then((response)=>{
-            setProduto(response.data.estoque[0]);
-        })
-        .catch((erro)=> console.log(erro))
-    }, []);
-   
+export default function InputBusca({onChange, labelName, name, id}){
     return(
-        <ContainerBusca>
-            <InputBusca/>
-        </ContainerBusca>
-    );
-};
+        <>
+            <label htmlFor={id}>{labelName}</label>
+            <input type="text" name={name} id={id} onChange={onChange} required />
+        </>
+    )
+}
 
-export default InputBuscaId;
+// function InputBuscaId({id, onSubmit, type, value}) {
+//     const [produto, setProduto]  = useState([]);
+//     const navigate = useNavigate();
+//     const {id} = useParams();
+    
+//     useEffect(()=>{
+//         api.get(`/id/{id}`)
+//         .then((response)=>{
+//             setProduto(response.data.estoque[0]);
+//         })
+//         .catch((erro)=> console.log(erro))
+//     }, []);
+   
+//     return(
+//         <ContainerBusca>
+//             <InputBusca/>
+//         </ContainerBusca>
+//     );
+// };
+
+// export default InputBuscaId;
