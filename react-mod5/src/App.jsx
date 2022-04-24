01ee3c30c0f023b5da70711e3399f88d470d5931
sucoDeGoiaba <flp.pxxs@gmail.com>
2022-04-24 18:39:52 -0300

    feat: adicionando rota /adicionar para post

--- react-mod5/src/App.jsx
+++ react-mod5/src/App.jsx
@@ -1,8 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"; 
+
 import AddProd from "./pages/addProduto/AddProd";
 
 function App() {
   return (
-   <AddProd/>
+  //  <AddProd/>
+  <Router>
+    {/* Entra a parte do Fabiano */}
+    <Routes>
+      <Route path="/"/>
+      <Route path="/adicionar" element={ <AddProd/> }/>
+    </Routes>
+  </Router>
 
   );
 }
