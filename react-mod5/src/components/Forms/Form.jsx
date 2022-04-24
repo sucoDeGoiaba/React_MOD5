0818f544ae127abc5529f9a34e2e3076293d4f5a
fabianobarroli <fabianobarroli@gmail.com>
2022-04-24 08:27:57 -0300

    feat (components\input) alterando codigo

--- react-mod5/src/components/Forms/Form.jsx
+++ react-mod5/src/components/Forms/Form.jsx
@@ -1,2 +1,21 @@
 import { useState } from "react";
-import {api} from "../../services/api.js"
+import {api} from "../../services/api.js";
+import InputBusca from "../Input/InputBusca.jsx";
+
+export default function Form (){
+    const [prodInfo, setProdInfo] = useState({});
+    function onChange(e){
+        setProdInfo({...prodInfo, [e.target.name]: e.target.value})
+    }
+    
+    function submit(e){
+        e.preventDefault()
+        api.get("/id", prodInfo)
+    }
+
+    return(
+        <form onSubmit={submit}>
+            <InputBusca 
+        </form>
+    )
+}
