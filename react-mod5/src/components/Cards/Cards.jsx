366fa0622800a5836f048424df33a37fa16fe4cb
Vivian Caroline M <viviancaroliner@gmail.com>
2022-04-28 13:57:58 -0300

    feat: adicionando loader

--- react-mod5/src/components/Cards/Cards.jsx
+++ react-mod5/src/components/Cards/Cards.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./CardsSytled.jsx";
 import { Link } from "react-router-dom";
 
-import styled from "./Card.module.css"
+import styled from "./Card.module.css";
 
-export default function Cards({nomeDoItem, id, tipo, quantidade, preco}) {
-    return (
-        <Link className={styled.tirandoUnderline} to={`/consulta/${id}`}>
-            <Container>
-                <div className={styled.diagramacao}><p>{ nomeDoItem }</p> <p>ID: {id}</p></div>
-                <p>Tipo: {tipo}</p>
-                <div className={styled.diagramacao}><p>R${preco}</p> <p>Un.: {quantidade}</p></div>
-            </Container>
-        </Link>
+export default function Cards({
+  nomeDoItem,
+  id,
+  tipo,
+  quantidade,
+  preco,
+  isLoading,
+}) {
+  const [loading, setLoading] = useState(true);
 
-    )
+  useEffect(() => {
+    isLoading(loading);
+  });
+  return (
+    <Link className={styled.tirandoUnderline} to={`/consulta/${id}`}>
+      <Container isLoading={(value) => setLoading(value)}>
+        <div className={styled.diagramacao}>
+          <p>{nomeDoItem}</p> <p>ID: {id}</p>
+        </div>
+        <p>Tipo: {tipo}</p>
+        <div className={styled.diagramacao}>
+          <p>R${preco}</p> <p>Un.: {quantidade}</p>
+        </div>
+      </Container>
+    </Link>
+  );
 }
--- react-mod5/src/components/listaDeItens/ListaDeItens.jsx
+++ react-mod5/src/components/listaDeItens/ListaDeItens.jsx
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 import { api } from "../../services/api.js";
 import Cards from "../Cards/Cards.jsx";
+import MyLoader from "../Loader/Loader.jsx";
 
-import styled from "./ListaDeItens.module.css"
+import styled from "./ListaDeItens.module.css";
 
 export default function MostraProduto() {
-    const [produto, setProduto] = useState([]);
+  const [produto, setProduto] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        api.get("/produtos")
-            .then((response) => {
-                setProduto(response.data.estoque);
-                console.log("produto", produto);
-            })
-            .catch((erro) => console.log(erro));
-    });
+  useEffect(() => {
+    api
+      .get("/produtos")
+      .then((response) => {
+        setProduto(response.data.estoque);
+        console.log("produto", produto);
+      })
+      .catch((erro) => console.log(erro));
+  });
 
-    return (
-        <ul className={styled.lista}>
-            {produto.map((produto) => {
-                return (
-                    <Cards
-                        key={produto.id}
-                        nomeDoItem={produto.nome}
-                        id={produto.id}
-                        tipo={produto.tipo}
-                        quantidade={produto.quantidade}
-                        preco={produto.preco}
-                    />
-                );
-            })}
-        </ul>
-    )
+  return (
+    <ul className={styled.lista}>
+      {produto.map((produto) => {
+        return (
+          <div>
+            {loading && (
+              <>
+                <MyLoader />
+                <MyLoader />
+                <MyLoader />
+                <MyLoader />
+              </>
+            )}
+            <Cards
+              key={produto.id}
+              nomeDoItem={produto.nome}
+              id={produto.id}
+              tipo={produto.tipo}
+              quantidade={produto.quantidade}
+              preco={produto.preco}
+              isLoading={(value) => setLoading(value)}
+            />
+          </div>
+        );
+      })}
+    </ul>
+  );
 }
